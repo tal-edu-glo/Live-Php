84963943af5f68da4ba2b64758b18cde210b1a3f
金弘扬 <jinhongyang@100tal.com>
2020-03-18 08:57:55 +0800

    添加 唤起客户端地址

---
+++ src/GLOLive/PullAgreement.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace GLOLive;
+
+use GLOLive\Utils\Request;
+
+class PullAgreement extends Base
+{
+    const STUDENT_TOLE = 0;
+    const TEACHER_ROLE = 1;
+
+    /**
+     * 获取唤起客户端地址
+     * @param $classID int 北斗直播课节id
+     * @param $userID string 业务方用户id
+     * @param $userRole int 业务方用户id
+     * @param $headImg string 讲师头像地址
+     * @param $userName string 用户名
+     * @return bool|mixed
+     */
+    public function GetAgreementUrl($classID,$userID,$userRole,$headImg,$userName,$teacherName=''){
+        $url = sprintf("%s/pan/convert/get", $this->url);
+        $query = [
+            'class_id' => $classID,
+            'user_id'=>$userID,
+            'user_role'=>$userRole,
+            'head_img'=>$headImg,
+            'user_name'=>$userName,
+        ];
+        if (!empty($teacherName)){
+            $query['teacher_name'] = $teacherName;
+        }
+        return Request::get($url, $this->signAuth->handel($query));
+    }
+
+
+    /**
+     * 获取唤起讲师端地址
+     * @param $classID int 北斗直播课节id
+     * @param $userID string 业务方用户id
+     * @param $headImg string 讲师头像地址
+     * @param $userName string 用户名
+     * @return bool|mixed
+     */
+    public function GetTeacherAgreementUrl($classID,$userID,$headImg,$userName){
+        return $this->GetAgreementUrl($classID,$userID,self::TEACHER_ROLE,$headImg,$userName);
+    }
+
+    /**
+     * 获取唤起用户端地址
+     * @param $classID int 北斗直播课节id
+     * @param $userID string 业务方用户id
+     * @param $headImg string 讲师头像地址
+     * @param $userName string 用户名
+     * @param $teacherName string 讲师名称
+     * @return bool|mixed
+     */
+    public function GetStudentAgreementUrl($classID,$userID,$headImg,$userName,$teacherName){
+        return $this->GetAgreementUrl($classID,$userID,self::STUDENT_TOLE,$headImg,$userName,$teacherName);
+    }
+
+
+
+}
